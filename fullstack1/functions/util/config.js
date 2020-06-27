e08ba383f3aa465f90ae7ea5ae1597f7badec191
sidd2476 <siddhukanu1@gmail.com>
2020-06-28 02:00:27 +0530

    initial commit

---
+++ fullstack1/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDpHryDXBXii70y8D9OabPHvw8cKVNb69U",
+    authDomain: "alarm-757a1.firebaseapp.com",
+    databaseURL: "https://alarm-757a1.firebaseio.com",
+    projectId: "alarm-757a1",
+    storageBucket: "alarm-757a1.appspot.com",
+    messagingSenderId: "236068650848",
+    appId: "1:236068650848:web:cfec8c5ca82ff974c22805"
+  };
